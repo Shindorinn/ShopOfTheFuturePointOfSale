c92c52cfef501259365ef1f364f648c31ce19604
aelex13 <du.a.meijer@googlemail.com>
2014-01-28 15:12:28 +0100

    Added JSONParser(credits to Pieter) and the Domain Objects(credits to Arz for the orignals).

--- src/main/java/org/futureworks/shopofthefuture/pointofsale/domain/ShoppingListItem.java
+++ src/main/java/org/futureworks/shopofthefuture/pointofsale/domain/ShoppingListItem.java
@@ -1,5 +1,60 @@
 package org.futureworks.shopofthefuture.pointofsale.domain;
 
-public class ShoppingListItem {
+public class ShoppingListItem{
+	
+	private final String barcode;
+	
+	private String name;
+	private double price;
+	
+	public ShoppingListItem(String barcode, String name, double price) {
+		this.barcode = barcode;
+		this.name = name;
+		this.price = price;
+	}
+	
+	/**
+	 * Sets a new name for this ShoppingItem
+	 * @param name, the new name for this ShoppingItem
+	 */
+	public void setName(String name) {
+		if(name != null && !name.equals("")){
+			this.name = name;
+		}
+	}
+	
+	/**
+	 * Sets a new price for this ShoppingItem
+	 * @param price, the new price for this ShoppingItem
+	 */
+	public void setPrice(double price) {
+		if(price >= 0) {
+			this.price = price;
+		}
+	}
+	
+	/**
+	 * Returns the barcode of this ShoppingItem
+	 * @return String, barcode
+	 */
+	public String getBarcode() {
+		return barcode;
+	}
+	
+	/**
+	 * Returns the name of this ShoppingItem
+	 * @return String, name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Returns the price of this ShoppingItem
+	 * @return Double, price
+	 */
+	public Double getPrice() {
+		return price;
+	}
 
 }
